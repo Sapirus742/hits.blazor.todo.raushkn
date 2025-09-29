d3c72099e217fc3c1441cfe4971d3e8f1eea0346
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2025-09-29 19:33:32 +0500

    Fix - Finish Build

---
+++ TodoServerApp/Data/Migrations/20250929141018_NewMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 19, 10, 17, 752, DateTimeKind.Local).AddTicks(3166));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 19, 10, 17, 752, DateTimeKind.Local).AddTicks(3193));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 19, 10, 17, 752, DateTimeKind.Local).AddTicks(3196));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 19, 10, 17, 752, DateTimeKind.Local).AddTicks(3198));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 15, 8, 16, 707, DateTimeKind.Local).AddTicks(6520));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 15, 8, 16, 707, DateTimeKind.Local).AddTicks(6548));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 15, 8, 16, 707, DateTimeKind.Local).AddTicks(6551));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 9, 29, 15, 8, 16, 707, DateTimeKind.Local).AddTicks(6553));
+        }
+    }
+}
--- TodoServerApp/Data/Services/MSSQLDataService.cs
+++ TodoServerApp/Data/Services/MSSQLDataService.cs
@@ -10,19 +10,34 @@ namespace TodoServerApp.Data.Services
             return await context.TaskItems.ToArrayAsync();
         }
 
+        public async Task AddAsync(TaskItem taskItem)
+        {
+            await context.TaskItems.AddAsync(taskItem);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TaskItem taskItem)
+        {
+            var local = context.TaskItems.Local.FirstOrDefault(x => x.Id == taskItem.Id);
+            if(local != null)
+            {
+                context.Entry(local).State = EntityState.Detached;
+            }
+
+            context.TaskItems.Update(taskItem);
+            await context.SaveChangesAsync();
+        }
+        
         public async Task SaveAsync(TaskItem taskItem)
         {
             if (taskItem.Id == 0)
             {
-                taskItem.CreatedDate = DateTime.Now;
-                await context.TaskItems.AddAsync(taskItem);
+                await AddAsync(taskItem);
             }
             else
             {
-                context.TaskItems.Update(taskItem);
+                await UpdateAsync(taskItem);
             }
-
-            await context.SaveChangesAsync();
         }
 
         public async Task<TaskItem> GetTaskAsync(int id)
